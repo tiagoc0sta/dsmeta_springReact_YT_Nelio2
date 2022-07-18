43744b4e1942cd0d07727af25827aea50723cf0e
Tiago Costa <tiagodpv@gmail.com>
2022-07-17 21:09:36 -0400

    add Twillo SMS

--- Projetos/dsmeta/backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ Projetos/dsmeta/backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,38 +1,49 @@
 package com.devsuperior.dsmeta.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-	@Service
-	public class SmsService {
+@Service
+public class SmsService {
 
-		@Value("${twilio.sid}")
-		private String twilioSid;
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
 	
-		@Value("${twilio.key}")
-		private String twilioKey;
+	@Autowired
+	private SaleRepository saleRepository;
 
-		@Value("${twilio.phone.from}")
-		private String twilioPhoneFrom;
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue()+"/" + sale.getDate().getYear();
+		
+		String msg = "The seller " + sale.getSellerName() + " was the best in "+ date
+				+ " selling a total $" + String.format( "%.2f" , sale.getAmount()) ;
 
-		@Value("${twilio.phone.to}")
-		private String twilioPhoneTo;
+		Twilio.init(twilioSid, twilioKey);
 
-		public void sendSms() {
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-			Twilio.init(twilioSid, twilioKey);
+		Message message = Message.creator(to, from, msg).create();
 
-			PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-			PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
-
-			Message message = Message.creator(to, from, "Teste").create();
-
-			System.out.println(message.getSid());
-		}
+		System.out.println(message.getSid());
 	}
-
-
+}
