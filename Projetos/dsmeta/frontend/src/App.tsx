c456ad2024c6f03c96bcd0053859fbb2a1bbdd22
Tiago Costa <tiagodpv@gmail.com>
2022-07-18 20:08:42 -0400

    add Toast

--- Projetos/dsmeta/frontend/src/App.tsx
+++ Projetos/dsmeta/frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header/Index";
 import SalesCard from "./components/SalesCard/Index";
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
